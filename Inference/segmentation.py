03fc93a71698e315f3986c7ce07ba0f2fc4c6279
saada865 <saadm865@gmail.com>
2024-02-26 11:49:56 +0500

    segmentation.py refactoring

--- Inference/segmentation.py
+++ Inference/segmentation.py
@@ -1,8 +1,18 @@
 from ultralytics import YOLO
 
 
-model = YOLO('../yolov8n-seg.pt')
+def segment(img_path):
+    """
+    This function performs image segmentation on the user image provided.
 
-results = model("img/testImg.jpg", save=True)
+    param (img_path):
+    This is the user provided image file path.
+    """
+    model = YOLO('../yolov8n-seg.pt')
 
-print(results)
+    results = model(img_path, save=True)
+
+    print(results)
+
+
+segment("img/bigstock-Kids-Play-Football-Cute-Littl-471646067.jpg")
